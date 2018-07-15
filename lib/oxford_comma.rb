1f406f5c181f0a171cde18df7ff7d60c3181b4b5
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-15 01:40:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,9 @@ def oxford_comma(array)
 
 if array.length > 2
 return array[0..-2].join(', ') + " and " + array[-1] 
-elsif
+else
   return array[0..1].join(" and ")
-else 
-  return array
+
 end
 
 end
